12a9f34d6aecc3041137debbd5b694dbf82d4ea6
Shubhankar Singh <singhshubhankar01@gmail.com>
2024-09-16 15:27:41 +0530

    View Lecture

--- src/components/course/StreamLecture.jsx
+++ src/components/course/StreamLecture.jsx
@@ -10,10 +10,10 @@ const StreamLecture = () => {
     console.log(video)
 
     return (
-        <div class="my-5">
+        <div className="my-5">
             <h2>{video.title}</h2>
             <video name='demo' controls width='600px' height='300px'>
-                <source src={`http://localhost:8080/${video.url}`} type="video/mp4"></source>
+                <source src={`http://localhost:8080/courses/play/${video.id}`} type="video/mp4" alt="Lecture"></source>
             </video>
         </div>
     )
